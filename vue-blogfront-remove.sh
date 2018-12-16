f98a5379a4b0394f9ef05d7d08d1bf1fbe9590fd
Nathan Seymour <mail@nathanseymour.com>
2018-12-16 11:23:04 -0500

    More CLI fixes

--- vue-blogfront-remove.sh
+++ vue-blogfront-remove.sh
@@ -15,9 +15,14 @@ echo -e "This includes all posts, users and other content from the blog."
 
 echo -e "\n${RED}Remove $1 only at YOUR OWN RISK!${NC}\n"
 
-echo -e "To confirm that you have read and fully understand and agree to the above statements.\n"
+echo -e "To confirm that you have read and fully understand and agree to the above statements, please type the following numbers: ${NUMBER}.\n"
 
 read -p "Confirmation: " UserInput
+
+if [[ ! $UserInput = $NUMBER ]]; then
+    echo -e "\n${RED}Your input is incorrect. Please fully read the above statements and try again${NC}"
+    exit
+fi
 
 if [ -d /opt/ambrest/blogs/$1 ]; then
     cd /opt/ambrest/blogs/$1
